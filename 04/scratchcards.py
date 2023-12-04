021baa37a7539a86fcfac8a44ef297a8489185af
dek <dekonnection@gmail.com>
2023-12-04 08:49:17 +0100

    merge both parts for day 4

--- 04/scratchcards.py
+++ 04/scratchcards.py
@@ -3,9 +3,9 @@ import logging
 import re
 
 
-## part 1 start ##
-def part1():
-    pile_points = 0
+def scratchcards():
+    part1_pile_points = 0
+    part2_scratchcards = []
     with open("payload.txt") as f:
         for line in f:
             numbers = line.split(":")[1].strip().split("|")
@@ -14,45 +14,25 @@ def part1():
             wins = len(
                 [number for number in drawn_numbers if number in winning_numbers]
             )
+
+            # part 1
             card_points = 0
             if wins:
                 card_points = pow(2, wins - 1)
 
-            pile_points += card_points
+            part1_pile_points += card_points
+            part2_scratchcards.append({"count": 1, "wins": wins})
 
-    logging.debug(f"Result for part 1 is: {pile_points}")
-    return pile_points
-
-
-## part 1 end ##
-
-
-## part 2 start ##
-def part2():
-    scratchcards = []
-    with open("payload.txt") as f:
-        for line in f:
-            numbers = line.split(":")[1].strip().split("|")
-            winning_numbers = re.findall("[0-9]+", numbers[0])
-            drawn_numbers = re.findall("[0-9]+", numbers[1])
-
-            wins = len(
-                [number for number in drawn_numbers if number in winning_numbers]
-            )
-            scratchcards.append({"count": 1, "wins": wins})
-
-    for index, card in enumerate(scratchcards):
+    for index, card in enumerate(part2_scratchcards):
         for _ in range(0, card["count"]):
             for card_to_copy in range(index + 1, index + 1 + card["wins"]):
-                scratchcards[card_to_copy]["count"] += 1
+                part2_scratchcards[card_to_copy]["count"] += 1
 
-    total_cards = sum([card["count"] for card in scratchcards])
+    part2_total_cards = sum([card["count"] for card in part2_scratchcards])
 
-    logging.debug(f"Result for part 2 is: {total_cards}")
-    return total_cards
-
-
-## part 2 end ##
+    logging.debug(f"Result for part 1 is: {part1_pile_points}")
+    logging.debug(f"Result for part 2 is: {part2_total_cards}")
+    return (part1_pile_points, part2_total_cards)
 
 
 if __name__ == "__main__":
@@ -63,8 +43,7 @@ if __name__ == "__main__":
         format="%(asctime)s [%(levelname)s] %(message)s",
     )
 
-    result_part_1 = part1()
-    result_part_2 = part2()
+    results = scratchcards()
 
-    print(f"Result for part 1: {result_part_1}")
-    print(f"Result for part 2: {result_part_2}")
+    print(f"Result for part 1: {results[0]}")
+    print(f"Result for part 2: {results[1]}")
