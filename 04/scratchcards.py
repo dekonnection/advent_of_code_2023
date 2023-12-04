496d0e0733d5db379cf9f787eabb02db82640c30
dek <dekonnection@gmail.com>
2023-12-04 08:38:31 +0100

    remove debug

--- 04/scratchcards.py
+++ 04/scratchcards.py
@@ -7,20 +7,10 @@ import re
 def part1():
     pile_points = 0
     with open("payload.txt") as f:
-        logging.info("Payload reading: start.")
-        # we iterate on each line of the payload
-        for line_id, raw_line in enumerate(f):
-            game_id = line_id + 1
-            line = raw_line.strip()  # some cleaning just in case
-            logging.debug(f"Payload reading new line: `{line}`")
-
+        for line in f:
             numbers = line.split(":")[1].strip().split("|")
             winning_numbers = re.findall("[0-9]+", numbers[0])
             drawn_numbers = re.findall("[0-9]+", numbers[1])
-            logging.debug(
-                f"Card {game_id}: winning numbers {winning_numbers}, drawn numbers {drawn_numbers}"
-            )
-
             wins = len(
                 [number for number in drawn_numbers if number in winning_numbers]
             )
@@ -28,7 +18,6 @@ def part1():
             if wins:
                 card_points = pow(2, wins - 1)
 
-            logging.debug(f"Card {1}: {card_points} points")
             pile_points += card_points
 
     logging.debug(f"Result for part 1 is: {pile_points}")
@@ -40,21 +29,12 @@ def part1():
 
 ## part 2 start ##
 def part2():
-    total_scratchcards = 0
     scratchcards = []
     with open("payload.txt") as f:
-        logging.info("Payload reading: start.")
-        # we iterate on each line of the payload
-        for line_id, raw_line in enumerate(f):
-            game_id = line_id + 1
-            line = raw_line.strip()  # some cleaning just in case
-            logging.debug(f"Payload reading new line: `{line}`")
+        for line in f:
             numbers = line.split(":")[1].strip().split("|")
             winning_numbers = re.findall("[0-9]+", numbers[0])
             drawn_numbers = re.findall("[0-9]+", numbers[1])
-            logging.debug(
-                f"Card {game_id}: winning numbers {winning_numbers}, drawn numbers {drawn_numbers}"
-            )
 
             wins = len(
                 [number for number in drawn_numbers if number in winning_numbers]
